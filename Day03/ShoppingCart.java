f02f0c99de336b65fc35f6d08430ba8b2dec6f72
Nivedita Vishwakarma <niveditavishwakarma205@gmail.com>
2025-01-20 16:39:59 +0530

    Shopping Cart program completed

---
+++ Day03/ShoppingCart.java
@@ -0,0 +1,62 @@
+public class ShoppingCart
+{
+	public static void main(String args[])
+	{
+		// Create Product objects
+        Product product1 = new Product("Laptop", 50000, 2);
+        Product product2 = new Product("Smartphone", 30000, 3);
+
+        // Use instanceof to check object type
+        if (product1 instanceof Product) {
+            product1.displayDetails();  // Display details of product1
+        }
+
+        if (product2 instanceof Product) {
+            product2.displayDetails();  // Display details of product2
+        }
+
+        // Update the discount globally
+        Product.updateDiscount(15.0);
+
+        // Display product details after discount update
+        System.out.println("After updating discount to 15%:");
+        if (product1 instanceof Product) {
+            product1.displayDetails();  
+        }
+
+        if (product2 instanceof Product) {
+            product2.displayDetails(); 
+        }
+	}
+}
+class Product {
+    
+    static double discount = 10.0; 
+
+    // Instance variables
+    String productName;
+    double price;
+    int quantity;
+	
+    public Product(String productName, double price, int quantity) {
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // Static method to update the discount percentage for all products
+    public static void updateDiscount(double newDiscount) {
+        discount = newDiscount;
+    }
+
+    // Method to display product details
+    public void displayDetails() {
+        System.out.println("Product Name: " + productName);
+        System.out.println("Price: " + price);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("Discount: " + discount + "%");
+        System.out.println("Final Price after discount: " + (price - (price * discount / 100)) * quantity);
+        System.out.println();
+    }
+}
+
